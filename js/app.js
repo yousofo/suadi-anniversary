748acd2a770ba3fcc418d9e58229134664c4e8a9
yousefn <jon772002@gmail.com>
2024-09-11 09:03:31 +0300

    api integration

--- js/app.js
+++ js/app.js
@@ -1,9 +1,60 @@
+let currentItem = {
+  name: null,
+  price: null,
+};
+let paymentOption = null;
 document.querySelectorAll(".apply-button").forEach((button) => {
   button.addEventListener("click", () => {
+    currentItem = {
+      name: button.getAttribute("data-name"),
+      price: button.getAttribute("data-price"),
+    };
     document.querySelector(".payment-wrapper").classList.add("active");
   });
 });
 
 document.querySelector("#closePayment").addEventListener("click", () => {
   document.querySelector(".payment-wrapper").classList.remove("active");
+});
+
+document.querySelectorAll("input[type=radio]").forEach((radio) => {
+  radio.addEventListener("click", () => {
+    paymentOption = radio.value;
+  });
+});
+
+async function handlePayment() {
+  try {
+    const result = await fetch(
+      "https://masartech.com.sa/api/ViewsCourses/PayWithoutSaveData",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          studentModel: {
+            Name: document.querySelector("#name").value,
+            PhoneNumbe: document.querySelector("#phone").value,
+            Email: document.querySelector("#email").value,
+            IdentityNumber: document.querySelector("#id").value,
+            Password: document.querySelector("#password").value,
+          },
+          PackageName: currentItem.name,
+          Amount: currentItem.price,
+          IsTabby: paymentOption === "tabby",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await result.json();
+    window.location.href = data.redirect_url;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.querySelector("#paymentForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  handlePayment();
 });
