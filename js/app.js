4be7f3c4472cf1cff1b1884fd2855bcacd6d3fc1
yousefn <jon772002@gmail.com>
2024-09-11 05:40:21 +0300

    ف

--- js/app.js
+++ js/app.js
@@ -1,30 +1,9 @@
-const allPaymentOptions = [
-  {
-    image: "/media/logos/payment/tamara.png",
-    text: "Tamara",
-  },
-  {
-    image: "/media/logos/payment/master-card.png",
-    text: "Mastercard",
-  },
-  {
-    image: "/media/logos/payment/apple-pay.png",
-    text: "Apple Pay",
-  },
-  {
-    image: "/media/logos/payment/mada-pay.jpg",
-    text: "mada Pay",
-  },
-  {
-    image: "/media/logos/payment/tabby-pay.png",
-    text: "Tabby",
-  },
-];
 document.querySelectorAll(".apply-button").forEach((button) => {
   button.addEventListener("click", () => {
     document.querySelector(".payment-wrapper").classList.add("active");
   });
 });
+
 document.querySelector("#closePayment").addEventListener("click", () => {
   document.querySelector(".payment-wrapper").classList.remove("active");
 });
